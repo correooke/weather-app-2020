9eb4e31e506e08ddaf4fb9906772e3a06c7aac95
HSNBDES33\eocariz <eocariz@hasar.com>
2020-03-22 18:44:23 -0300

    agrego estilo a CityInfo

--- src/components/CityInfo/CityInfo.jsx
+++ src/components/CityInfo/CityInfo.jsx
@@ -5,8 +5,8 @@ import Typography from '@material-ui/core/Typography'
 const CityInfo = ({ city, country }) => {
     return (
         <>
-            <Typography>{city}</Typography>
-            <Typography>{country}</Typography>
+            <Typography display="inline" variant="h4">{city}, </Typography>
+            <Typography display="inline" variant="h6">{country}</Typography>
         </>
     )
 }
