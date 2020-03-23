d7487dfec74a49e736001c16ebe7c5731c720ec8
HSNBDES33\eocariz <eocariz@hasar.com>
2020-03-23 09:53:34 -0300

    Agrego el assert a CityInfo

--- src/components/CityInfo/CityInfo.test.jsx
+++ src/components/CityInfo/CityInfo.test.jsx
@@ -6,11 +6,13 @@ test("CityInfo render", async () => {
     // AAA
     // Arrange 
     // Act
+    const city = "Buenos Aires"
+    const country = "Argentina"
 
     // Render: renderiza el componente y retorna una serie de funciones de utilidad
     // En este caso utilizamos "findAllByRole" para "consultar" a nuestro componente
     // Vamos a analizar su estado en el "Assert"
-    const { findAllByRole } = render(<CityInfo city="Buenos Aires" country="Argentina" />)
+    const { findAllByRole } = render(<CityInfo city={city} country={country} />)
 
     // Assert
     // findAllByRole nos va a buscar (en este caso) todos los componentes que sean 
@@ -18,5 +20,13 @@ test("CityInfo render", async () => {
     // El resultado es un array de componentes (cityAndCountryComponents)
     const cityAndCountryComponents = await findAllByRole("heading")
 
-    // ????
+    // ¿Cuando el test va a ser correcto?
+    // DEfinición: 
+    // Cuando en el primer elemento (heading) se encuentre la ciudad "Buenos Aires"
+    // y cuando en el segundo elemento se encuentre el pais Argentina
+
+    expect(cityAndCountryComponents[0]).toHaveTextContent(city)
+    expect(cityAndCountryComponents[1]).toHaveTextContent(country)
+
+    // Si estas condiciones se cumplen (expect), el test esta "ok"
 })
