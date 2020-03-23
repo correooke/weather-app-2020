4e7e6a156fb5821885a26a721048d59be66fcc69
HSNBDES33\eocariz <eocariz@hasar.com>
2020-03-22 23:34:55 -0300

    Arrange and act en el test de CityInfo

--- src/components/CityInfo/CityInfo.test.jsx
+++ src/components/CityInfo/CityInfo.test.jsx
@@ -1,7 +1,22 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import CityInfo from './CityInfo' // SUT: Subject under testing (objeto del testeo)
 
 test("CityInfo render", async () => {
     // AAA
     // Arrange 
     // Act
+
+    // Render: renderiza el componente y retorna una serie de funciones de utilidad
+    // En este caso utilizamos "findAllByRole" para "consultar" a nuestro componente
+    // Vamos a analizar su estado en el "Assert"
+    const { findAllByRole } = render(<CityInfo city="Buenos Aires" country="Argentina" />)
+
     // Assert
+    // findAllByRole nos va a buscar (en este caso) todos los componentes que sean 
+    // "heading" => H1, H2, H3.. etc
+    // El resultado es un array de componentes (cityAndCountryComponents)
+    const cityAndCountryComponents = await findAllByRole("heading")
+
+    
 })
