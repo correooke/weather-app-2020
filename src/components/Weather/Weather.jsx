1f959fcc780015f0bca4d1ed4703899fe292ff80
HSNBDES33\eocariz <eocariz@hasar.com>
2020-03-24 16:13:40 -0300

    Agrego validación de OneOf a weather

--- src/components/Weather/Weather.jsx
+++ src/components/Weather/Weather.jsx
@@ -8,6 +8,14 @@ import { WiCloud,
     WiRain } from 'react-icons/wi'
 import { IconContext } from 'react-icons'
 
+const validValues = [
+        "cloud",
+        "cloudy",
+        "fog",
+        "sunny",
+        "rain",
+    ]
+
 const stateByName = {
     cloud: WiCloud,
     cloudy: WiDayCloudy,
@@ -35,7 +43,7 @@ const Weather = ({ temperature, state }) => {
 
 Weather.propTypes = {
     temperature: PropTypes.number.isRequired,
-    state: PropTypes.string.isRequired,
+    state: PropTypes.oneOf(validValues).isRequired,
 }
 
 export default Weather
