f88cf78630d722135488a56f83e4102da3a53423
HSNBDES33\eocariz <eocariz@hasar.com>
2020-03-23 22:17:36 -0300

    Agrego CityInfo y Weather a WeatherList

--- src/components/CityList/CityList.jsx
+++ src/components/CityList/CityList.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import CityInfo from './../CityInfo/CityInfo'
+import Weather from './../Weather/Weather'
 
+// li: es un item (según tag html)
+const renderCityAndCountry = cityAndCountry => {
+    const { city, country } = cityAndCountry
+
+    return (
+        <li>
+            <CityInfo city={city} country={country} />
+            <Weather temperature={10} state="sunny" />
+        </li>
+    )
+}
+
+// cities: es un array, y en cada item tiene que tener la ciudad, pero además el country
+// ul: tag html para listas no ordenadas
 const CityList = ({ cities }) => {
     return (
-        <div>
-            CityList
-        </div>
+        <ul>
+            {
+                cities.map(cityAndCountry => renderCityAndCountry(cityAndCountry))
+            }
+        </ul>
     )
 }
 
--- src/components/CityList/CityList.stories.jsx
+++ src/components/CityList/CityList.stories.jsx
@@ -6,4 +6,11 @@ export default {
     component: CityList
 }
 
-export const CityListExample = () => <CityList cities={[]} />
+const cities = [
+    { city: "Buenos Aires", country: "Argentina"},
+    { city: "Bogotá", country: "Colombia"},
+    { city: "Madrid", country: "España"},
+    { city: "Ciudad de México", country: "México"},
+]
+
+export const CityListExample = () => <CityList cities={cities} />
