293070402989dd7a29927fdaa9f9bc3a21487bba
HSNBDES33\eocariz <eocariz@hasar.com>
2020-03-24 22:50:57 -0300

    Agrego test unitario a WeatherDetails

--- src/components/WeatherDetails/WeatherDetails.test.jsx
+++ src/components/WeatherDetails/WeatherDetails.test.jsx
@@ -1,4 +1,16 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import WeatherDetails from './WeatherDetails'
 
-test("WeatherDetails render", () => {
-    
+// findByText: Permite encontrar un componente por el texto que muestra
+test("WeatherDetails render", async () => {
+    const { findByText } = render(<WeatherDetails wind={10} humidity={80} />)
+
+    // Al utilizar las barras utilizamos un REGEXP, una expresión regular
+    const wind = await findByText(/10/)
+
+    const humidity = await findByText(/80/)
+
+    expect(wind).toHaveTextContent("Viento: 10 km/h")
+    expect(humidity).toHaveTextContent("Humedad: 80%")
 })
