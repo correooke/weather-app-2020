554b16c998a5b49abc306ca7ff085deed11da1d7
HSNBDES33\eocariz <eocariz@hasar.com>
2020-03-28 12:17:11 -0300

    Creación de stories para Forecast

---
+++ src/components/Forecast/Forecast.stories.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Forecast from './Forecast'
+
+export default {
+    title: "Forecast",
+    component: Forecast
+}
+
+const forecastItemList = [
+	{ hour: 18, state:"sunny", temperature:17, weekDay:"Jueves" },
+	{ hour: 6, state:"cloud", temperature:18, weekDay:"Viernes" },
+	{ hour: 12, state:"fog", temperature:18, weekDay:"Viernes" },
+	{ hour: 18, state:"cloudy", temperature:19, weekDay:"Viernes" },
+	{ hour: 14, state:"rain", temperature:17, weekDay:"Sábado" },
+	{ hour: 14, state:"rain", temperature:17, weekDay:"Sábado" }, 
+]
+
+export const ForecastExample = () => (<Forecast forecastItemList={forecastItemList} />)
