cf1302860b15dbfe2703d260e3ef6deb38772b4b
HSNBDES33\eocariz <eocariz@hasar.com>
2020-03-28 18:55:32 -0300

    Agrego el LineChart al ForecastChart

--- src/components/ForecastChart/ForecastChart.jsx
+++ src/components/ForecastChart/ForecastChart.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { LineChart, 
+        Line, 
+        CartersianGrid, 
+        XAxis, 
+        YAxis,
+        Tooltip,
+        Legend } from 'recharts'
 
 const ForecastChart = ({ data }) => {
     return (
         <div>
-            ForecastChart
+            <LineChart
+                height={250}
+                width={700}
+                margin={{ top: 20, bottom: 20, left: 5, right: 5 }}
+                data={data} >
+                <XAxis dataKey="dayHour"></XAxis>
+                <YAxis></YAxis>
+                <CartersianGrid></CartersianGrid>
+                <Tooltip></Tooltip>
+                <Legend></Legend>
+                <Line type="monotone" dataKey="max" stroke="#FF0000"></Line>
+                <Line type="monotone" dataKey="min" stroke="#0000FF"></Line>
+            </LineChart>
         </div>
     )
 }
