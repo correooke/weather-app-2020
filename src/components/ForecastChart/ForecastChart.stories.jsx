1de55dd5b28899a010b0da4abae34105fa20a23f
HSNBDES33\eocariz <eocariz@hasar.com>
2020-03-28 18:25:49 -0300

    Agregamos data de ejemplo para ForecastChart (ok)

--- src/components/ForecastChart/ForecastChart.stories.jsx
+++ src/components/ForecastChart/ForecastChart.stories.jsx
@@ -12,37 +12,37 @@ export default {
 // max: Temperatura máxima
 const data = [
     {
-        "day-hour": "Jue 18",
+        "dayHour": "Jue 18",
         "min": 14,
         "max": 22,
     },
     {
-        "day-hour": "Vie 06",
+        "dayHour": "Vie 06",
         "min": 18,
         "max": 27,
     },
     {
-        "day-hour": "Vie 12",
+        "dayHour": "Vie 12",
         "min": 18,
         "max": 28,
     },
     {
-        "day-hour": "Vie 18",
+        "dayHour": "Vie 18",
         "min": 18,
         "max": 25,
     },
     {
-        "day-hour": "Sab 06",
+        "dayHour": "Sab 06",
         "min": 15,
         "max": 22,
     },
     {
-        "day-hour": "Sab 12",
+        "dayHour": "Sab 12",
         "min": 12,
         "max": 19,
     }
 ]
 
 export const ForecastChartExample = () => (
-    <ForecastChart forecastData={data} />
+    <ForecastChart data={data} />
 )
