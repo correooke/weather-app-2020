67d056068755a606bcf19ed7209bc933101f5093
HSNBDES33\eocariz <eocariz@hasar.com>
2020-03-28 14:44:05 -0300

    ForecastChart

---
+++ src/components/ForecastChart/ForecastChart.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const ForecastChart = props => {
+    return (
+        <div>
+            ForecastChart
+        </div>
+    )
+}
+
+ForecastChart.propTypes = {
+
+}
+
+export default ForecastChart
---
+++ src/components/ForecastChart/ForecastChart.stories.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ForecastChart from './ForecastChart'
+
+export default {
+    title: "ForecastChart",
+    component: ForecastChart
+}
+
+export const ForecastChartExample = () => (
+    <ForecastChart />
+)
