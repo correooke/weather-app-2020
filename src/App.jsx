5fc49c5f78b07640869fedd3cd4d57c82bdcb3bb
HSNBDES33\eocariz <eocariz@hasar.com>
2020-03-29 17:26:47 -0300

    Utilización de Link en el contexto de la app

--- src/App.jsx
+++ src/App.jsx
@@ -10,22 +10,28 @@ const App = () => {
             <h1>App</h1>
          
             <Router>
-
-                <div>
-                    <Link to="/main">Ir a Main</Link>
-                </div>  
                 <Switch>
                     <Route exact path="/">
                         Welcome
+                        <div>
+                            <Link to="/main">Ir a Main</Link>
+                        </div>  
                     </Route>
                     <Route path="/main">
                         Main
                     </Route>      
                     <Route path="/city">
                         City
+                        <div>
+                            <Link to="/main">Volver a Main</Link>
+                        </div>  
+
                     </Route> 
                     <Route>
                         Not Found
+                        <div>
+                            <Link to="/main">Volver a Main</Link>
+                        </div>  
                     </Route>                                                           
                 </Switch>
             </Router>
