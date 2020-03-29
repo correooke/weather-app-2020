d35de0c46d64c959bf56813b85b5d8a526bfa635
HSNBDES33\eocariz <eocariz@hasar.com>
2020-03-29 18:48:40 -0300

    Utilización de useHistory

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 
 const MainPage = () => {
+    const history = useHistory()
+
+    const onClickHandler = () => {
+        // history.push permite alterar la URL por programación
+        history.push("/city")
+    }
+
     return (
         <div>
             MainPage
+            <button onClick={onClickHandler}>Ir a City Page</button>
         </div>
     )
 }
