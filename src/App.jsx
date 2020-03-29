f49e2ab56e37ff2ed64875e3d96019b5503a7adf
HSNBDES33\eocariz <eocariz@hasar.com>
2020-03-29 17:10:52 -0300

    Presentacion de Link

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import { BrowserRouter as Router,
     Switch, 
-    Route } from 'react-router-dom'
+    Route,
+    Link } from 'react-router-dom'
 
 const App = () => {
     return (
         <div>
             <h1>App</h1>
+         
             <Router>
+
+                <div>
+                    <Link to="/main">Ir a Main</Link>
+                </div>  
                 <Switch>
                     <Route exact path="/">
                         Welcome
