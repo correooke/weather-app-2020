5621417d1745280dcd09f171210c2e8643a38aaa
HSNBDES33\eocariz <eocariz@hasar.com>
2020-03-29 16:56:01 -0300

    Agrego Router a App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import { BrowserRouter as Router,
+    Switch, 
+    Route } from 'react-router-dom'
 
 const App = () => {
     return (
         <div>
-            App
+            <h1>App</h1>
+            <Router>
+                <Switch>
+                    <Route exact path="/">
+                        Welcome
+                    </Route>
+                    <Route path="/main">
+                        Main
+                    </Route>      
+                    <Route path="/city">
+                        City
+                    </Route> 
+                    <Route>
+                        Not Found
+                    </Route>                                                           
+                </Switch>
+            </Router>
         </div>
     )
 }
