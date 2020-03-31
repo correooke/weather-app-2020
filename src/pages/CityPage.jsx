9273fb1e7ccf2a977086a2be57ab1a2edc1650a8
HSNBDES33\eocariz <eocariz@hasar.com>
2020-03-31 20:33:47 -0300

    Creación de page city

--- src/pages/CityPage.jsx
+++ src/pages/CityPage.jsx
@@ -1,14 +1,87 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Grid from '@material-ui/core/Grid'
+import CityInfo from './../components/CityInfo'
+import Weather from './../components/Weather'
+import WeatherDetails from './../components/WeatherDetails'
+import ForecastChart from './../components/ForecastChart'
+import Forecast from './../components/Forecast'
+
+const dataExample = [
+    {
+        "dayHour": "Jue 18",
+        "min": 14,
+        "max": 22,
+    },
+    {
+        "dayHour": "Vie 06",
+        "min": 18,
+        "max": 27,
+    },
+    {
+        "dayHour": "Vie 12",
+        "min": 18,
+        "max": 28,
+    },
+    {
+        "dayHour": "Vie 18",
+        "min": 18,
+        "max": 25,
+    },
+    {
+        "dayHour": "Sab 06",
+        "min": 15,
+        "max": 22,
+    },
+    {
+        "dayHour": "Sab 12",
+        "min": 12,
+        "max": 19,
+    }
+]
+
+const forecastItemListExample = [
+	{ hour: 18, state:"sunny", temperature:17, weekDay:"Jueves" },
+	{ hour: 6, state:"cloud", temperature:18, weekDay:"Viernes" },
+	{ hour: 12, state:"fog", temperature:18, weekDay:"Viernes" },
+	{ hour: 18, state:"cloudy", temperature:19, weekDay:"Viernes" },
+	{ hour: 6, state:"rain", temperature:17, weekDay:"Sábado" },
+	{ hour: 12, state:"rain", temperature:17, weekDay:"Sábado" }, 
+]
 
 const CityPage = () => {
+    const city = "Buenos Aires"
+    const country = "Argentina"
+    const state = "cloudy"
+    const temperature = 20
+    const humidity = 80
+    const wind = 5
+    const data = dataExample
+    const forecastItemList = forecastItemListExample
+
     return (
-        <div>
-            City
-            <div>
-                <Link to="/main">Volver a Main</Link>
-            </div>  
-        </div>
+        <Grid container
+            justify="center"
+            direction="column">
+            <Grid item xs={12}>
+                <CityInfo city={city} country={country} />
+            </Grid>
+            <Grid container item xs={12}>
+                <Grid item xs={8}>
+                    <Weather state={state} temperature={temperature} />
+                </Grid>
+                <Grid item xs={4}>
+                    <WeatherDetails humidity={humidity} 
+                        wind={wind} />
+                </Grid>
+            </Grid>
+            <Grid item>
+                <ForecastChart data={data} />
+            </Grid>
+            <Grid item>
+                <Forecast forecastItemList={forecastItemList} />
+            </Grid>
+        </Grid>        
     )
 }
 
