f28f093b88c00a4cc8f55e4fbdf079e4cc1e0ba3
HSNBDES33\eocariz <eocariz@hasar.com>
2020-04-04 12:41:30 -0300

    Hago responsive Forecast

--- src/components/Weather/Weather.jsx
+++ src/components/Weather/Weather.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Grid from '@material-ui/core/Grid'
 import Typography from '@material-ui/core/Typography'
 import { IconContext } from 'react-icons'
 import IconState, { validValues } from './../IconState'
 
 const Weather = ({ temperature, state }) => {
     return (
-        <div>
-            <IconContext.Provider value={{ size:'5em'}}>
+        <Grid container item
+            direction="row"
+            justify="center"
+            alignItems="center"
+            spacing={1}>
+            <IconContext.Provider value={{ size:'6em'}}>
                 <IconState state={state} />
             </IconContext.Provider>
-            <Typography display="inline" variant="h3">{temperature}</Typography>
-        </div>
+            <Typography display="inline" variant="h2">{temperature}</Typography>
+        </Grid>
     )
 }
 
