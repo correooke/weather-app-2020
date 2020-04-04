0f7f2191fccb03c3cfec7d1c87dfc9a696faa45d
HSNBDES33\eocariz <eocariz@hasar.com>
2020-04-04 12:17:57 -0300

    Hago responsive el ForecastChart

--- src/components/ForecastChart/ForecastChart.jsx
+++ src/components/ForecastChart/ForecastChart.jsx
@@ -6,14 +6,13 @@ import { LineChart,
         XAxis, 
         YAxis,
         Tooltip,
-        Legend } from 'recharts'
+        Legend, 
+        ResponsiveContainer } from 'recharts'
 
 const ForecastChart = ({ data }) => {
     return (
-        <div>
+        <ResponsiveContainer height={250} width={"95%"}>
             <LineChart
-                height={250}
-                width={700}
                 margin={{ top: 20, bottom: 20, left: 5, right: 5 }}
                 data={data} >
                 <XAxis dataKey="dayHour"></XAxis>
@@ -24,7 +23,7 @@ const ForecastChart = ({ data }) => {
                 <Line type="monotone" dataKey="max" stroke="#FF0000"></Line>
                 <Line type="monotone" dataKey="min" stroke="#0000FF"></Line>
             </LineChart>
-        </div>
+        </ResponsiveContainer>
     )
 }
 
