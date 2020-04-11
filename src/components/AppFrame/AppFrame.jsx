fa41c54b556e2384ddf2a61a2dd4d5402b96e187
HSNBDES33\eocariz <eocariz@hasar.com>
2020-04-11 17:16:20 -0300

    Agrego estructura de AppFrame

--- src/components/AppFrame/AppFrame.jsx
+++ src/components/AppFrame/AppFrame.jsx
@@ -1,11 +1,45 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import AppBar from '@material-ui/core/AppBar'
+import IconButton from '@material-ui/core/IconButton'
+import { IconContext } from 'react-icons'
+import Grid from '@material-ui/core/Grid'
+import Link from '@material-ui/core/Link'
+import { Link as LinkRouter } from 'react-router-dom'
+import Typography from '@material-ui/core/Typography'
+import Toolbar from '@material-ui/core/Toolbar'
+import { WiDaySunny } from 'react-icons/wi'
 
 const AppFrame = props => {
     return (
-        <div>
-            AppFrame
-        </div>
+        <Grid container
+            justify="center">
+            <AppBar position="static">
+                <Toolbar variant="dense">
+                    <IconButton color="inherit" aria-label="menu">
+                        <Link 
+                            component={LinkRouter}
+                            to="/main" 
+                            color="inherit" 
+                            aria-label="menu">
+                            <IconContext.Provider value={{size:'2em'}}>
+                                <WiDaySunny />
+                            </IconContext.Provider>
+                        </Link>
+                    </IconButton>
+                    <Typography variant="h6" color="inherit">
+                        Weather App
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <Grid container item
+                xs={12}
+                sm={11}
+                md={10}
+                bg={8}>
+                loren ipusum loren ipusumloren ipusumloren ipusumloren ipusumloren ipusumloren ipusumloren ipusumloren ipusumloren ipusumloren ipusumloren ipusumloren ipusum
+            </Grid>
+        </Grid>
     )
 }
 
--- src/components/AppFrame/AppFrame.stories.jsx
+++ src/components/AppFrame/AppFrame.stories.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import AppFrame from './AppFrame'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 export default {
     title: "AppFrame",
     component: AppFrame
 }
 
-export const AppFrameExample = () => <AppFrame />
+export const AppFrameExample = () => (
+    <Router>
+        <AppFrame />
+    </Router>
+)
