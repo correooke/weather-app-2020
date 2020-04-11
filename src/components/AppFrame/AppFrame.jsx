67bf6b7c7215a0aad2e8cd0779bbe63708cbf241
HSNBDES33\eocariz <eocariz@hasar.com>
2020-04-11 13:38:27 -0300

    Creación de AppFrame

---
+++ src/components/AppFrame/AppFrame.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const AppFrame = props => {
+    return (
+        <div>
+            AppFrame
+        </div>
+    )
+}
+
+AppFrame.propTypes = {
+
+}
+
+export default AppFrame
---
+++ src/components/AppFrame/AppFrame.stories.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import AppFrame from './AppFrame'
+
+export default {
+    title: "AppFrame",
+    component: AppFrame
+}
+
+export const AppFrameExample = () => <AppFrame />
