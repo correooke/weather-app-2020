20f1108887f4839138d4e0873db6c7385215a77e
HSNBDES33\eocariz <eocariz@hasar.com>
2020-04-12 16:03:21 -0300

    Utilización de useRef

--- src/components/WelcomeScreen/WelcomeScreen.jsx
+++ src/components/WelcomeScreen/WelcomeScreen.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 import PropTypes from 'prop-types'
 
 const WelcomeScreen = ({ children }) => {
+    const myRefDiv = useRef(null) // Valor inicial
+    
+    // En la primera renderización "myRefDiv.current" es igual 
+    // a "nulo", el valor inicial
+    console.log("myRefDiv.current", myRefDiv.current) // x 2
+
+    // La función de useEffect se va "invocar" ante 
+    // la segunda renderización, en ese punto ya va a 
+    // contener un valor "myRefDiv.current"
+    useEffect(() => {
+        console.log("myRefDiv.current (en UseEffect)", myRefDiv.current)
+    })
+
     return (
-        <div>
+        <div ref={myRefDiv}>
             WelcomeScreen
         </div>
     )
