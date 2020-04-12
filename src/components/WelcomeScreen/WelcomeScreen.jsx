e40eca421e7f02bb742a42b02ced310bce4b9c28
HSNBDES33\eocariz <eocariz@hasar.com>
2020-04-12 16:33:04 -0300

    Preparo la inicialización de "vanta"

--- src/components/WelcomeScreen/WelcomeScreen.jsx
+++ src/components/WelcomeScreen/WelcomeScreen.jsx
@@ -1,9 +1,10 @@
-import React, { useRef, useEffect } from 'react'
+import React, { useRef, useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 
 const WelcomeScreen = ({ children }) => {
     const myRefDiv = useRef(null) // Valor inicial
-    
+    const [vanta, setVanta] = useState(0) // vanta va a ser inicializado en "0"
+
     // En la primera renderización "myRefDiv.current" es igual 
     // a "nulo", el valor inicial
     console.log("myRefDiv.current", myRefDiv.current) // x 2
@@ -13,7 +14,18 @@ const WelcomeScreen = ({ children }) => {
     // contener un valor "myRefDiv.current"
     useEffect(() => {
         console.log("myRefDiv.current (en UseEffect)", myRefDiv.current)
-    })
+
+        // Sólo pasa una vez por dentro del if
+        // vanta === 0, es igual a "vanta == false" 
+        // ... aún más corto, es igual a "!vanta"
+        if (!vanta) {
+            // SOLO PASA UNA VEZ 
+            // Acá vamos a hacer la inicialización de "vanta"
+            setVanta(1) // vanta = 1
+            console.log("Establezco vanta a un valor diferente de 0")
+        }
+    }, [vanta]) // Con esto me aseguro que siga funcionando bien
+    // si tuviera más variables "use"
 
     return (
         <div ref={myRefDiv}>
