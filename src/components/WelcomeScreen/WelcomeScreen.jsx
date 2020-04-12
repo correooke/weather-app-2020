7a4458ddb5c1ab1ad614f4d19af35d90b1dfdc82
HSNBDES33\eocariz <eocariz@hasar.com>
2020-04-12 15:41:27 -0300

    Creación de componente WelcomeScreen

---
+++ src/components/WelcomeScreen/WelcomeScreen.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const WelcomeScreen = ({ children }) => {
+    return (
+        <div>
+            WelcomeScreen
+        </div>
+    )
+}
+
+WelcomeScreen.propTypes = {
+    children: PropTypes.node,
+}
+
+export default WelcomeScreen
---
+++ src/components/WelcomeScreen/WelcomeScreen.stories.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import WelcomeScreen from './WelcomeScreen'
+
+export default {
+    title: "WelcomeScreen",
+    component: WelcomeScreen
+}
+
+export const WelcomeScreenExample = () => (
+    <WelcomeScreen />
+)
