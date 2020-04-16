e5bb6372213c2e179ac41a1a0105a804824dd093
HSNBDES33\eocariz <eocariz@hasar.com>
2020-04-15 21:29:00 -0300

    Agregamos zona de resaltado del Welcome

--- src/pages/WelcomePage.jsx
+++ src/pages/WelcomePage.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import WelcomeScreen from './../components/WelcomeScreen'
+import Grid from '@material-ui/core/Grid'
 
 const WelcomePage = () => {
     return (
         <WelcomeScreen>
-
+            <Grid container
+                direction="column"
+                justify="center"
+                className="full">
+                <div className="highlight">
+                    
+                </div>
+            </Grid>
         </WelcomeScreen>
     )
 }
