797854844c6ce354acef463ef8860ee3bb241857
HSNBDES33\eocariz <eocariz@hasar.com>
2020-04-19 20:39:05 -0300

    Mejoro la pantalla de bienvenida

--- src/pages/WelcomePage.jsx
+++ src/pages/WelcomePage.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import WelcomeScreen from './../components/WelcomeScreen'
 import Grid from '@material-ui/core/Grid'
+import Typography from '@material-ui/core/Typography'
+import { IconContext } from 'react-icons'
+import { WiDaySunny } from 'react-icons/wi'
 
 const WelcomePage = () => {
     return (
@@ -10,7 +13,24 @@ const WelcomePage = () => {
                 justify="center"
                 className="full">
                 <div className="highlight">
-                    
+                    <Grid item container xs={12}
+                        justify="center"
+                        alignItems="center">
+                        <Grid item>
+                            <IconContext.Provider value={{ size:"6em" }}>
+                                <WiDaySunny />
+                            </IconContext.Provider>
+                        </Grid>
+                        <Grid item
+                            container
+                            direction="column"
+                            justify="center"
+                            alignItems="center">
+                            <Typography variant="h4" color="inherit">
+                                Weather App
+                            </Typography>
+                        </Grid>
+                    </Grid>
                 </div>
             </Grid>
         </WelcomeScreen>
