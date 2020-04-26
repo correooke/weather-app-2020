ae40598b4899e4400eb63df1a4e1203a52e58874
HSNBDES33\eocariz <eocariz@hasar.com>
2020-04-26 20:10:46 -0300

    Mejoramos la página 404

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,14 +1,45 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import Grid from '@material-ui/core/Grid'
+import Link from '@material-ui/core/Link'
+import Typography from '@material-ui/core/Typography'
+import { Link as RouterLink } from 'react-router-dom'
+import { IconContext } from 'react-icons'
+import { WiRain } from 'react-icons/wi'
 
 const NotFoundPage = () => {
     return (
-        <div>
-            Not Found
-            <div>
-                <Link to="/main">Volver a Main</Link>
-            </div>              
-        </div>
+        <Grid container
+            direction="column"
+            justify="center"
+            className="full">
+            <div className="highlight">
+                <Grid item container xs={12}
+                    justify="center"
+                    alignItems="center">
+                    <Grid item>
+                        <IconContext.Provider value={{ size:"6em" }}>
+                            <WiRain />
+                        </IconContext.Provider>
+                    </Grid>
+                    <Grid item
+                        container
+                        direction="column"
+                        justify="center"
+                        alignItems="center">
+                        <Typography variant="h4" color="inherit">
+                            404 | La página no existe
+                        </Typography>
+                        <Link color="inherit"
+                            aria-label="menu"
+                            component={RouterLink}
+                            to="/main">
+                            Volver al inicio
+                        </Link>
+                    </Grid>
+                </Grid>
+            </div>
+        </Grid>
+
     )
 }
 
