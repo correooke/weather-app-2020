fdc0c3f8b955f4d9a96a81989b1bdf6fbfb3c68e
HSNBDES33\eocariz <eocariz@hasar.com>
2020-06-17 19:41:16 -0300

    agrego Alert y manejo el error

--- src/components/CityList/CityList.jsx
+++ src/components/CityList/CityList.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import axios from 'axios'
 import convertUnits from 'convert-units'
+import Alert from '@material-ui/lab/Alert'
 import Grid from '@material-ui/core/Grid'
 import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
@@ -52,6 +53,7 @@ const CityList = ({ cities, onClickCity }) => {
       }
     */
     const [allWeather, setAllWeather] = useState({})
+    const [error, setError] = useState(null)
 
     useEffect(() => {
         const setWeather = (city, country, countryCode) => {
@@ -88,10 +90,13 @@ const CityList = ({ cities, onClickCity }) => {
                     const { data, status } = error.response
                     console.log("data", data)
                     console.log("status", status)
+                    setError("Ha ocurrido un error en el servidor del clima")
                 } else if (error.request) { // Errores que suceden por no llegar al server
                     console.log("Server in-accesible o no tengo internet")
+                    setError("Verifique la conexión a internet")
                 } else { // Errores imprevistos
                     console.log("Errores imprevistos")
+                    setError("Error al cargar los datos")
                 }
                 
             })
@@ -106,12 +111,18 @@ const CityList = ({ cities, onClickCity }) => {
     //const weather = { temperature: 10, state: "sunny" }
 
     return (
-        <List>
+        <div>
             {
-                cities.map(cityAndCountry => renderCityAndCountry(onClickCity)(cityAndCountry, 
-                    allWeather[`${cityAndCountry.city}-${cityAndCountry.country}`]))
+                error && <Alert onClose={() => setError(null)} severity="error">{error}</Alert>
             }
-        </List>
+            <List>
+                {
+                    cities.map(cityAndCountry => renderCityAndCountry(onClickCity)(cityAndCountry, 
+                        allWeather[`${cityAndCountry.city}-${cityAndCountry.country}`]))
+                }
+            </List>
+        </div>
+
     )
 }
 
