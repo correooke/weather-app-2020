541ab010258b11dbff992ff274cb00368ede9d05
HSNBDES33\eocariz <eocariz@hasar.com>
2020-06-26 19:51:44 -0300

    hook useMemo

--- src/hooks/useCityList.jsx
+++ src/hooks/useCityList.jsx
@@ -8,6 +8,7 @@ const useCityList = (cities) => {
     const [error, setError] = useState(null)
 
     useEffect(() => {
+        debugger
         const setWeather = async (city, countryCode) => {
 
             const url = getWeatherUrl({city, countryCode})
--- src/pages/CityPage.jsx
+++ src/pages/CityPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import Grid from '@material-ui/core/Grid'
 import LinearProgress from '@material-ui/core/LinearProgress'
 import AppFrame from './../components/AppFrame'
@@ -15,7 +15,9 @@ import { getCountryNameByCountryCode } from './../utils/serviceCities'
 const CityPage = () => {
     const { city, countryCode, chartData, forecastItemList } = useCityPage()
 
-    const { allWeather } = useCityList([{ city, countryCode }])
+    const cities = useMemo(() => ([{ city, countryCode }]), [city, countryCode])
+
+    const { allWeather } = useCityList(cities)
 
     const weather = allWeather[getCityCode(city, countryCode)]
 
