b916a4588b0413b908ec20ec6f1b835f7c242977
HSNBDES33\eocariz <eocariz@hasar.com>
2020-07-10 11:48:54 -0300

    encapsulo el contexto en WeatherContext

--- src/App.jsx
+++ src/App.jsx
@@ -6,59 +6,28 @@ import WelcomePage from './pages/WelcomePage'
 import MainPage from './pages/MainPage'
 import CityPage from './pages/CityPage'
 import NotFoundPage from './pages/NotFoundPage'
-import { WeatherStateContext, 
-    WeatherDispatchContext } from './WeatherContext'
-
-const initialValue = {
-    allWeather: {},
-    allChartData: {}, 
-    allForecastItemList: {}
-}
+import { WeatherContext } from './WeatherContext'
 
 const App = () => {
-
-    const reducer = useCallback((state, action) => {
-        switch (action.type) {
-            case 'SET_ALL_WEATHER':
-                const weatherCity = action.payload
-                const newAllWeather = { ...state.allWeather, ...weatherCity }
-                return { ...state, allWeather: newAllWeather }
-            case 'SET_CHART_DATA':
-                const chartDataCity = action.payload 
-                const newAllChartData = { ...state.allChartData, ...chartDataCity }
-                return { ...state, allChartData: newAllChartData }
-            case 'SET_FORECAST_ITEM_LIST':
-                const forecastItemListCity = action.payload
-                const newAllForecastItemListCity = { ...state.allForecastItemList, ...forecastItemListCity }
-                return { ...state, allForecastItemList: newAllForecastItemListCity }
-            default:
-                return state 
-        }
-    }, [])
-
-    const [state, dispatch] = useReducer(reducer, initialValue)
-
     return (
-        <WeatherDispatchContext.Provider value={dispatch}>
-            <WeatherStateContext.Provider value={state}>
-                <Router>
-                    <Switch>
-                        <Route exact path="/">
-                            <WelcomePage />
-                        </Route>
-                        <Route path="/main">
-                            <MainPage />
-                        </Route>      
-                        <Route path="/city/:countryCode/:city">
-                            <CityPage />
-                        </Route> 
-                        <Route>
-                            <NotFoundPage />
-                        </Route>                                                           
-                    </Switch>
-                </Router>
-            </WeatherStateContext.Provider>
-        </WeatherDispatchContext.Provider>
+        <WeatherContext>
+            <Router>
+                <Switch>
+                    <Route exact path="/">
+                        <WelcomePage />
+                    </Route>
+                    <Route path="/main">
+                        <MainPage />
+                    </Route>      
+                    <Route path="/city/:countryCode/:city">
+                        <CityPage />
+                    </Route> 
+                    <Route>
+                        <NotFoundPage />
+                    </Route>                                                           
+                </Switch>
+            </Router>
+        </WeatherContext>
     )
 }
 
--- src/WeatherContext.jsx
+++ src/WeatherContext.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 
 
 const WeatherStateContext = React.createContext()
@@ -6,7 +6,43 @@ const WeatherStateContext = React.createContext()
 const WeatherDispatchContext = React.createContext()
 
 
+const initialValue = {
+    allWeather: {},
+    allChartData: {}, 
+    allForecastItemList: {}
+}
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'SET_ALL_WEATHER':
+            const weatherCity = action.payload
+            const newAllWeather = { ...state.allWeather, ...weatherCity }
+            return { ...state, allWeather: newAllWeather }
+        case 'SET_CHART_DATA':
+            const chartDataCity = action.payload 
+            const newAllChartData = { ...state.allChartData, ...chartDataCity }
+            return { ...state, allChartData: newAllChartData }
+        case 'SET_FORECAST_ITEM_LIST':
+            const forecastItemListCity = action.payload
+            const newAllForecastItemListCity = { ...state.allForecastItemList, ...forecastItemListCity }
+            return { ...state, allForecastItemList: newAllForecastItemListCity }
+        default:
+            return state 
+    }
+}
+
+const WeatherContext = ({children}) => {
+    const [state, dispatch] = useReducer(reducer, initialValue)
+    return (
+        <WeatherDispatchContext.Provider value={dispatch}>
+            <WeatherStateContext.Provider value={state}>
+                {children}
+            </WeatherStateContext.Provider>
+        </WeatherDispatchContext.Provider>    )
+}
+
 export { 
-    WeatherStateContext, 
-    WeatherDispatchContext
+    WeatherContext, 
+    WeatherDispatchContext, 
+    WeatherStateContext
 }
